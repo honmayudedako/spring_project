328e467d7a066d8bb1a85b33c66fe8eb66490010
本間瑠 <yudedako0213@gmail.com>
2023-10-12 21:22:48 +0900

    GET&POST01/02

--- src/main/java/com/example/demo/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/demo/calculator/controller/CalculatorController.java
@@ -1,37 +1,37 @@
-package com.example.demo.calculator.controller; // パッケージ宣言
+package com.example.demo.calculator.controller;
 
-import org.springframework.stereotype.Controller; // Controllerクラスをインポート
-import org.springframework.ui.Model; // Modelオブジェクトをインポート
-import org.springframework.web.bind.annotation.GetMapping; // GetMappingアノテーションをインポート
-import org.springframework.web.bind.annotation.PostMapping; // PostMappingアノテーションをインポート
-import org.springframework.web.bind.annotation.RequestParam; // RequestParamアノテーションをインポート
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.calculator.service.CalculatorService;
 
-@Controller // Controllerクラスを示す
-public class CalculatorController { // CalculatorControllerクラスの宣言
+@Controller
+public class CalculatorController {
 
-	private final CalculatorService calculatorService; // CalculatorServiceクラスのインスタンスを保持するためのcalculatorService変数の宣言
+	private final CalculatorService calculatorService;
 
-	public CalculatorController(CalculatorService calculatorService) { // CalculatorControllerクラスのコンストラクタ
-		this.calculatorService = calculatorService; // CalculatorServiceクラスのインスタンスを代入
-	} // これにより、CalculatorControllerクラス内のメソッドでcalculatorServiceインスタンスを使用できるようになる
-
-	@GetMapping("calculation") // HTTP GETリクエストが"calculation"パスに送信されたときに、以下のメソッドが呼び出されることを示す
-	public String showForm() { // showFormメソッドの宣言
-		return "calculator.html"; // ビューとして"calculator.html"を表示するように指定
+	public CalculatorController(CalculatorService calculatorService) {
+		this.calculatorService = calculatorService;
 	}
 
-	@PostMapping("calculation") // HTTP POSTリクエストが"calculate"パスに送信されたときに、以下のメソッドが呼び出されることを示す
-	public String calculation(@RequestParam("num1") int num1, // HTTPリクエストから名前が"num1"のパラメータを受け取り、int型のnum1変数に代入
-			@RequestParam("operator") String operator, // HTTPリクエストから名前が"operator"のパラメータを受け取り、String型のoperator変数に代入
-			@RequestParam("num2") int num2, // HTTPリクエストから名前が"num2"のパラメータを受け取り、int型のnum2変数に代入
-			Model model) { // Modelはビューにデータを渡すためのオブジェクト、modelはModelオブジェクトを表す変数名
+	@GetMapping("calculation")
+	public String showForm() {
+		return "calculator.html";
+	}
 
-		int result = calculatorService.calculate(num1, operator, num2); // フォームから受け取った3つの数値をcalculateメソッドの引数とし、実行結果をresult変数に代入
+	@PostMapping("calculation")
+	public String calculation(@RequestParam("num1") int num1,
+			@RequestParam("operator") String operator,
+			@RequestParam("num2") int num2,
+			Model model) {
 
-		model.addAttribute("result", result); // 結果resultを"result"(属性名)という名前でModelオブジェクトに追加(resultは属性値)
+		int result = calculatorService.calculate(num1, operator, num2);
 
-		return "calculator.html"; // ビューとして"calculator.html"を表示するように指定
+		model.addAttribute("result", result);
+
+		return "calculator.html";
 	}
 }
